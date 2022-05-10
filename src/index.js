a023143e215a9e11fe1e8eed7ca7eef2a2732c2e
Senouci94190 <91454310+Senouci94190@users.noreply.github.com>
2022-05-10 08:45:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Game from './Game';
+import './index.css';  
+
+ReactDom.render (
+  <Game/>,
+   document.getElementById('root')
+);
